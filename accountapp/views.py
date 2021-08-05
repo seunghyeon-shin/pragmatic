0daf394db94c614756400ee9ca778ee64e724b74
별 <seunghyeon.shin@vntgcorp.com>
2021-08-05 16:33:20 +0900

    django course 45 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -11,29 +11,9 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.views.generic.list import MultipleObjectMixin
 
 from .decorators import account_ownership_required
-from .models import HelloWorld
 from articleapp.models import Article
 
 has_ownership = [account_ownership_required, login_required]
-
-
-@login_required
-def hello_world(request):
-
-    if request.method == "POST":
-
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        hello_world_list = HelloWorld.objects.all()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
 
 class AccountCreateView(CreateView):
