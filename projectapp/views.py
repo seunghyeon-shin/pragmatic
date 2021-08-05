8604e66891f858161fc1b5515be23e8eba047c48
별 <seunghyeon.shin@vntgcorp.com>
2021-08-05 13:29:11 +0900

    django course 43 commit

--- projectapp/views.py
+++ projectapp/views.py
@@ -5,10 +5,15 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
 from projectapp.models import Project
 
 from projectapp.forms import ProjectCreationForm
+
+from articleapp.models import Article
+
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -21,10 +26,25 @@ class ProjectCreateView(CreateView):
     def get_success_url(self):
         return reverse('projectapp:detail', kwargs={'pk': self.object.pk})
 
-class ProjectDetailView(DetailView):
+
+class ProjectDetailView(DetailView, MultipleObjectMixin):
     model = Project
     context_object_name = 'target_project'
     template_name = 'projectapp/detail.html'
+
+    paginate_by = 25
+
+    def get_context_data(self, **kwargs):
+        project = self.object
+        user = self.request.user
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+
+        object_list = Article.objects.filter(project=self.get_object())
+        return super(ProjectDetailView, self).get_context_data(object_list=object_list,
+                                                               subscription=subscription,
+                                                               **kwargs)
 
 
 class ProjectListView(ListView):
