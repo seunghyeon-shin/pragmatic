939a5861c762ef7357b857a9c25f22761e1df8a7
별 <seunghyeon.shin@vntgcorp.com>
2021-08-07 11:17:34 +0900

    edit course commit

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -6,8 +6,8 @@ from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable text-left',
-                                                           'style': 'height: auto;'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'height: auto; text-align:left'}))
     project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
 
     class Meta:
